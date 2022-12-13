ad363d8835d34a0d05dc5df7cc29a6962f2129ff
Mkparu <mkparusomtochi26@gmail.com>
2022-12-13 15:38:14 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+                                /*Js file for Landing page */
+// silder functionality
+
+
+
+
+
+
+
+// dropdown functionality
+
+
+
+
+
+// hamburger menu functionality
+
+
+
+
+// carousel functionality
+
+
+console.log("ready to code");
